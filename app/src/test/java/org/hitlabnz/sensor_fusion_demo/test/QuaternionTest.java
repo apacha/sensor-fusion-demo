3a34c1c9e1e9d2522852374cfc46934f58804fb2
Alexander Pacha <alexander.pacha@gmail.com>
2016-05-01 22:23:17 +0200

    Added another test.

--- app/src/test/java/org/hitlabnz/sensor_fusion_demo/test/QuaternionTest.java
+++ app/src/test/java/org/hitlabnz/sensor_fusion_demo/test/QuaternionTest.java
@@ -26,5 +26,28 @@ public class QuaternionTest {
         assertThat(q.y(), is(equalTo(0.0f)));
         assertThat(q.z(), is(equalTo(0.0f)));
     }
+
+
+    @Test
+    public void quaternion_performSlerp_expectCorrectValues() throws Exception {
+
+        // Arrange
+        Quaternion q1 = new Quaternion();
+        Quaternion q2 = new Quaternion();
+        Quaternion q3 = new Quaternion();
+        q1.loadIdentityQuat();
+        q2.setXYZW(0.5f, 0.5f, 0.5f, 1);
+        q2.normalise();
+
+        // Act
+        q2.slerp(q1, q3, 0.5f);
+
+        // Assert
+        assertThat(q3.w(), is(not(equalTo(1.0f))));
+        assertThat(q3.x(), is(not(equalTo(0.0f))));
+        assertThat(q3.y(), is(not(equalTo(0.0f))));
+        assertThat(q3.z(), is(not(equalTo(0.0f))));
+    }
+
 }
 
